892236ddd54ca551b49701ad6c19664426a694f1
AlexPst-Dev <113551830+AlexPst-Dev@users.noreply.github.com>
2023-06-02 11:57:58 +0200

    wip: CRUD users

--- mq-front/scripts/popup.js
+++ mq-front/scripts/popup.js
@@ -13,6 +13,7 @@ function openPopup() {
   xhr.send();
 }
 
+// Ajout d'un utilisateur
 async function validate() {
   const firstName = document.querySelector('.popup input[name="firstName"]').value;
   const lastName = document.querySelector('.popup input[name="lastName"]').value;
@@ -35,6 +36,7 @@ async function validate() {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Accept': '*/*'
       },
       body: JSON.stringify(newUser),
     });
@@ -50,6 +52,50 @@ async function validate() {
   } catch (error) {
     // Une erreur s'est produite lors de la requête
     console.log('Une erreur s\'est produite lors de la création de l\'utilisateur:', error);
+  }
+}
+
+// Mise à jour d'un utilisateur
+async function updateUser() {
+  // Récupérer les valeurs des champs du formulaire
+  const firstName = document.querySelector('.popup input[name="firstName"]').value;
+  const lastName = document.querySelector('.popup input[name="lastName"]').value;
+  const address = document.querySelector('.popup input[name="address"]').value;
+  const birthDate = document.querySelector('.popup input[name="birthDate"]').value;
+  const zipCode = document.querySelector('.popup input[name="zipCode"]').value;
+  const city = document.querySelector('.popup input[name="city"]').value;
+
+  // Récupérer l'ID de l'utilisateur à modifier
+  // const userId = ...; // Remplacer ... par la logique pour obtenir l'ID de l'utilisateur
+
+  // Appeler le controller updateUser avec les données du formulaire
+  try {
+    const response = await fetch(`http://192.168.20.47:8080/api/v1/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        address,
+        birthDate,
+        zipCode,
+        city,
+      }),
+    });
+
+    if (response.ok) {
+      // Mise à jour réussie
+      closePopup();
+    } else {
+      // La mise à jour a échoué
+      alert('Erreur lors de la mise à jour de l\'utilisateur');
+    }
+  } catch (error) {
+    // Une erreur s'est produite lors de la requête
+    console.error(error);
+    alert('Une erreur s\'est produite lors de la mise à jour de l\'utilisateur');
   }
 }
 
